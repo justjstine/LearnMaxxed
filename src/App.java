50e814bb83ead29cbe15b37555248c18e695ccd7
chocu <johncalub393@gmail.com>
2025-05-28 17:51:20 +0800

    Trial Commit

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/Login/LoginPage.fxml"));
-        primaryStage.setTitle("LearnMaxxing - Login :)");
+        primaryStage.setTitle("LearnMaxxing");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
