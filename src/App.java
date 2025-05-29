e44a805eb56bd9a32c6433b65f1475c18d8de294
HeavensLi <heavenliwanag@gmail.com>
2025-05-29 14:26:27 +0800

    heavennotcommit

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/Login/FXML/LoginPage.fxml"));
-        primaryStage.setTitle("LearnMaxxing");
+        primaryStage.setTitle("Learn Maxxing");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
