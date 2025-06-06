d5d86cf33c16ec8f338987e989e91090e69dacda
chocu <johncalub393@gmail.com>
2025-06-06 13:12:04 +0800

    Learning materials done for chemistry subject

--- src/LearningMaterials/STEM/CHEMISTRY/Controllers/Chem1Chap1Controller.java
+++ src/LearningMaterials/STEM/CHEMISTRY/Controllers/Chem1Chap1Controller.java
@@ -1,66 +1,38 @@
-package User.Controllers;
+package LearningMaterials.STEM.CHEMISTRY.Controllers;
 
-import Data.Session;
-import Data.Students;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-public class StemDashController {
-
+public class Chem1Chap1Controller {
     @FXML
-    private Label date;
+    private Button dashboardButton;
 
-    @FXML
-    private Label usernameSidePanel;
-
-    @FXML
-    private Label welcomebackUsername;
-
-    @FXML
-    private JFXButton BiologyButton;
-
-    @FXML
-    private JFXButton ChemistryButton;
-
-    @FXML
-    private JFXButton PhysicsButton;
-
-    @FXML
-    private JFXComboBox<String> subjectComboBox;
-    
     @FXML
     private Button logoutButton;
 
-    public void displayName(String name) {
-        usernameSidePanel.setText(name);
-    }
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private JFXButton nextButton;
+
+    @FXML
+    private JFXComboBox<String> subjectComboBox;
 
     @FXML
     public void initialize() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yyyy");
-        date.setText(LocalDate.now().format(formatter));
-
-        Students student = Session.getLoggedInStudent();
-        if (student != null) {
-            usernameSidePanel.setText(student.getFirstName());
-            welcomebackUsername.setText("Welcome back, " + student.getFirstName() + "!");
-        } else {
-            welcomebackUsername.setText("Welcome back!");
-            usernameSidePanel.setText("");
-        }
-
+        Platform.runLater(() -> scrollPane.setVvalue(0));
         subjectComboBox.getItems().clear();
         subjectComboBox.getItems().addAll("Chemistry", "Physics", "Biology");
     }
@@ -81,13 +53,13 @@ public class StemDashController {
     }
 
     @FXML
-    public void chemistryButtonHandler(javafx.event.ActionEvent event) throws IOException {
+    public void dashboardButtonHandler(javafx.event.ActionEvent event) throws IOException {
         try {
             Stage stage = (Stage) logoutButton.getScene().getWindow();
             stage.close();
-            Parent root = FXMLLoader.load(getClass().getResource("/LearningMaterials/STEM/Chemistry/FXML/ChemChapter1.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/User/FXML/StemDashboard.fxml"));
             Stage newStage = new Stage();
-            newStage.setTitle("Chemistry");
+            newStage.setTitle("Login");
             newStage.setScene(new Scene(root, 1000, 600));
             newStage.show();
         } catch (Exception e) {
@@ -112,4 +84,25 @@ public class StemDashController {
             }
         }
     }
+
+    @FXML
+    public void nextButtonHandler(javafx.event.ActionEvent event) throws IOException {
+        try {
+            Stage stage = (Stage) logoutButton.getScene().getWindow();
+            stage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("/LearningMaterials/STEM/Chemistry/FXML/ChemChapter2.fxml"));
+            Stage newStage = new Stage();
+            newStage.setTitle("Login");
+            newStage.setScene(new Scene(root, 1000, 600));
+            newStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void scrollToTop() {
+        if (scrollPane != null) {
+        scrollPane.setVvalue(0);
+        }
+    }   
 }
